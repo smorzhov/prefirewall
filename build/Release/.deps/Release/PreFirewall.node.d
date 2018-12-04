cce7b6350651e2914266917cb5d035be8eab9896
smorzhov <smorzhov@gmail.com>
2018-12-04 13:01:21 +0300

    Removed unnecessary stuff

--- build/Release/.deps/Release/PreFirewall.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/PreFirewall.node := rm -rf "Release/PreFirewall.node" && cp -af "Release/obj.target/PreFirewall.node" "Release/PreFirewall.node"
--- build/Release/.deps/Release/obj.target/PreFirewall/cpp/PreFirewallSrc/IPAddresses/IPv4Address.o.d
+++
@@ -1,8 +0,0 @@
-cmd_Release/obj.target/PreFirewall/cpp/PreFirewallSrc/IPAddresses/IPv4Address.o := g++ '-DNODE_GYP_MODULE_NAME=PreFirewall' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/smorzhov/.node-gyp/5.11.0/include/node -I/home/smorzhov/.node-gyp/5.11.0/src -I/home/smorzhov/.node-gyp/5.11.0/deps/uv/include -I/home/smorzhov/.node-gyp/5.11.0/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -Wall -std=c++11 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/PreFirewall/cpp/PreFirewallSrc/IPAddresses/IPv4Address.o.d.raw   -c -o Release/obj.target/PreFirewall/cpp/PreFirewallSrc/IPAddresses/IPv4Address.o ../cpp/PreFirewallSrc/IPAddresses/IPv4Address.cpp
-Release/obj.target/PreFirewall/cpp/PreFirewallSrc/IPAddresses/IPv4Address.o: \
- ../cpp/PreFirewallSrc/IPAddresses/IPv4Address.cpp \
- ../cpp/PreFirewallSrc/IPAddresses/IPv4Address.h \
- ../cpp/PreFirewallSrc/IPAddresses/IPAddress.h
-../cpp/PreFirewallSrc/IPAddresses/IPv4Address.cpp:
-../cpp/PreFirewallSrc/IPAddresses/IPv4Address.h:
-../cpp/PreFirewallSrc/IPAddresses/IPAddress.h:
